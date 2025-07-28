3d34d9bd771f6b6a50bd032898d3a952356cba02
khushboo <khushi2002ra10@gmail.com>
2025-07-28 19:26:58 +0530

    Updated main.c to include sum, differnce, and multiplication

--- main.c
+++ main.c
@@ -4,7 +4,12 @@ int main()
 {
 int a = 10;
 int b = 20;
-int c = a + b;
-printf("sum of a and b is %d\n",c);
+int sum = a + b;
+int dif = a - b;
+int mul = a * b;
+printf("Sum of a and b is %d\n",sum);
+printf("Difference of a and b is %d\n",dif);
+printf("Multiplication of a and b is %d\n",mul);
+
 return 0;
 }
