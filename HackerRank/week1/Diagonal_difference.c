8bf7b33a7103c04af929c0bea5cd7ef97f0285a3
khushboo <khushi2002ra10@gmail.com>
2025-08-08 20:31:44 +0530

    Update Diagonal_difference.c with recent changes

--- HackerRank/week1/Diagonal_difference.c
+++ HackerRank/week1/Diagonal_difference.c
@@ -1,44 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int diagonalDifference(int arr_rows, int arr_columns, int** arr) {
-    int leftToRightSum = 0;
-    int rightToLeftSum = 0;
-    
-    for (int i = 0; i < arr_rows; i++) {
-        leftToRightSum += arr[i][i];
+int diagonalDifference(int size, int grid[size][size]) {
+    int leftDiagonalSum = 0, rightDiagonalSum = 0;
+
+    for (int row = 0; row < size; row++) {
+        leftDiagonalSum += grid[row][row];
+        rightDiagonalSum += grid[row][size - 1 - row];
     }
-    
-    for (int i = 0; i < arr_rows; i++) {
-        rightToLeftSum += arr[i][arr_rows - 1 - i];
-    }
-    
-    int difference = leftToRightSum - rightToLeftSum;
-    return abs(difference);
+
+    int difference = leftDiagonalSum - rightDiagonalSum;
+    return difference < 0 ? -difference : difference;
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
+    int dimension;
+    scanf("%d", &dimension);
 
-    int** arr = (int**)malloc(n * sizeof(int*));
-    for (int i = 0; i < n; i++) {
-        arr[i] = (int*)malloc(n * sizeof(int));
-    }
+    int grid[dimension][dimension]; 
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &arr[i][j]);
-        }
-    }
+    for (int r = 0; r < dimension; r++)
+        for (int c = 0; c < dimension; c++)
+            scanf("%d", &grid[r][c]);
 
-    int result = diagonalDifference(n, n, arr);
+    int result = diagonalDifference(dimension, grid);
     printf("%d\n", result);
-
-    for (int i = 0; i < n; i++) {
-        free(arr[i]);
-    }
-    free(arr);
 
     return 0;
 }
